297cd25cd922f76a62836098204cca963b2d4c92
mohamed farid <mohamed_hussein5@live.com>
2021-10-31 22:54:05 +0200

    refactor to use the DTO from common

--- src/main/java/com/iter/financial/FinancialApplication.java
+++ src/main/java/com/iter/financial/FinancialApplication.java
@@ -3,7 +3,7 @@ package com.iter.financial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.iter.financial", "com.iter.errorhandling" })
 public class FinancialApplication {
 
 	public static void main(String[] args) {
